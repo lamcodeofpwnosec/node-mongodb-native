a5e5e6db34afa97e9774bad6cbe63630c015200d
Warren James <warren.james@mongodb.com>
2024-06-21 12:06:30 -0400

    refactor(NODE-6187): refactor to use TimeoutContext abstraction (#4131)

--- src/operations/execute_operation.ts
+++ src/operations/execute_operation.ts
@@ -58,7 +58,11 @@ type ResultTypeFromOperation<TOperation> =
 export async function executeOperation<
   T extends AbstractOperation<TResult>,
   TResult = ResultTypeFromOperation<T>
+<<<<<<< HEAD
 >(client: MongoClient, operation: T, timeoutContext?: TimeoutContext | null): Promise<TResult> {
+=======
+>(client: MongoClient, operation: T, timeoutContext?: TimeoutContext): Promise<TResult> {
+>>>>>>> e04694c3b (refactor(NODE-6187): refactor to use TimeoutContext abstraction (#4131))
   if (!(operation instanceof AbstractOperation)) {
     // TODO(NODE-3483): Extend MongoRuntimeError
     throw new MongoRuntimeError('This method requires a valid operation instance');
@@ -81,6 +85,12 @@ export async function executeOperation<
   } else if (session.client !== client) {
     throw new MongoInvalidArgumentError('ClientSession must be from the same MongoClient');
   }
+
+  timeoutContext ??= TimeoutContext.create({
+    serverSelectionTimeoutMS: client.s.options.serverSelectionTimeoutMS,
+    waitQueueTimeoutMS: client.s.options.waitQueueTimeoutMS,
+    timeoutMS: operation.options.timeoutMS
+  });
 
   const readPreference = operation.readPreference ?? ReadPreference.primary;
   const inTransaction = !!session?.inTransaction();
@@ -113,7 +123,8 @@ export async function executeOperation<
       topology,
       timeoutContext,
       session,
-      readPreference
+      readPreference,
+      timeoutContext
     });
   } finally {
     if (session?.owner != null && session.owner === owner) {
